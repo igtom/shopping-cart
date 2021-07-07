3f25d7cd5e7e490db2eb15166c1aa9f362adc597
igtom <igtom.martin@gmail.com>
2021-07-07 20:46:27 +0200

    commit

---
+++ src/app/components/product-alerts/product-alerts.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../../interfaces/product';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.scss']
+})
+export class ProductAlertsComponent implements OnInit {
+  @Input() product: Product;
+  @Output() notfiy = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
